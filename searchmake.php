ae71ea3783fcfe466b2c07f9f738915ee6c0840d
Daniel <Daniel@DANIEL-PC>
2017-06-19 16:45:41 +0300

    Changed by Daniel

--- searchmake.php
+++ searchmake.php
@@ -4,81 +4,46 @@
 
     }
     #makeP_1 {
-        box-sizing: border-box;
-        color: rgb(255, 255, 255);
-        height: 50px;
-        text-align: center;
-        text-decoration: none solid rgb(255, 255, 255);
-        text-shadow: rgba(150, 150, 150, 0) 1px 1px 0px;
-        text-size-adjust: 100%;
-        column-rule-color: rgb(255, 255, 255);
-        perspective-origin: 595.266px 25px;
-        transform-origin: 595.266px 25px;
-        caret-color: rgb(255, 255, 255);
 
-        border: 0px none rgb(255, 255, 255);
-        border-radius: 5px 5px 5px 5px;
+        color: rgb(255, 255, 255);
+        text-align: center;
         font: normal normal normal normal 20px / 24px "Hammersmith One", sans-serif;
-        margin: 0px;
-        outline: rgb(255, 255, 255) none 0px;
-        padding: 0px 0px 0px 5px;
+
+        margin-top: -10px;
+        margin-bottom: 2px;
     }/*#makeP_1*/
 
     #makeA_2, #makeA_3, #makeA_5, #makeA_7, #makeA_9, #makeA_11 {
-        box-sizing: border-box;
+
         color: rgb(255, 240, 0);
-        text-align: center;
-        text-decoration: none solid rgb(255, 240, 0);
-        text-shadow: rgba(150, 150, 150, 0) 1px 1px 0px;
-        text-size-adjust: 100%;
-        column-rule-color: rgb(255, 240, 0);
-        caret-color: rgb(255, 240, 0);
-        border: 0px none rgb(255, 240, 0);
-        font: normal normal 900 normal 22px / 24px "Hammersmith One", sans-serif;
-        outline: rgb(255, 240, 0) none 0px;
-        transition: background-color 0.4s linear 0s, color 0.4s linear 0s;
+        font: normal normal 600 normal 20px / 24px "Hammersmith One", sans-serif;
     }/*#makeA_2, #makeA_3, #makeA_5, #makeA_7, #makeA_9, #makeA_11*/
 
     #makeA_4, #makeA_6, #makeA_10 {
-        box-sizing: border-box;
         color: rgb(255, 240, 0);
-        text-align: center;
-        text-decoration: none solid rgb(255, 240, 0);
-        text-shadow: rgba(150, 150, 150, 0) 1px 1px 0px;
-        text-size-adjust: 100%;
-        column-rule-color: rgb(255, 240, 0);
-        caret-color: rgb(255, 240, 0);
-        border: 0px none rgb(255, 240, 0);
-        font: normal normal 900 normal 22px / 24px "Hammersmith One", sans-serif;
-        outline: rgb(255, 240, 0) none 0px;
-        transition: background-color 0.4s linear 0s, color 0.4s linear 0s;
+        font: normal normal 600 normal 20px / 24px "Hammersmith One", sans-serif;
     }/*#makeA_4, #makeA_6, #makeA_10*/
 
     #makeA_8, #makeA_12 {
-        box-sizing: border-box;
         color: rgb(255, 240, 0);
-        text-align: center;
-        text-decoration: none solid rgb(255, 240, 0);
-        text-shadow: rgba(150, 150, 150, 0) 1px 1px 0px;
-        text-size-adjust: 100%;
-        column-rule-color: rgb(255, 240, 0);
-        caret-color: rgb(255, 240, 0);
-        border: 0px none rgb(255, 240, 0);
-        font: normal normal 900 normal 22px / 24px "Hammersmith One", sans-serif;
-        outline: rgb(255, 240, 0) none 0px;
-        transition: background-color 0.4s linear 0s, color 0.4s linear 0s;
+        font: normal normal 600 normal 20px / 24px "Hammersmith One", sans-serif;
+    }/*#makeA_8, #makeA_12*/
+      #makeA_13 {
+        color: #fff;
+        font: normal normal 600 normal 20px / 24px "Hammersmith One", sans-serif;
     }/*#makeA_8, #makeA_12*/
 
     @media only screen and (max-width:600px) {
 
         #searchbymake{
             height: 200px;
-        width: 100% !important;}
+            width: 100% !important;}
     }
 </style>
-<div class="row blue" id="searchbymake" style="margin-top: 20px;   width: 90%;border-radius: 10px;">
-    <h4 class="white-text" >Search by make</h4>
-    <p id="makeP_1">
-        <a href="cars.php?make=Subaru&amp;item=car" id="makeA_2">Subaru</a> | <a href="cars.php?make=Toyota&amp;item=car" id="makeA_3">Toyota</a> | <a href="cars.php?make=Jaguar&amp;item=car" id="makeA_4">Jaguar</a> | <a href="cars.php?make=Nissan&amp;item=car" id="makeA_5">Nissan</a> | <a href="cars.php?make=Chevrolet&amp;item=car" id="makeA_6">Chevrolet</a> | <a href="cars.php?make=Mercedes&amp;item=car" id="makeA_7">Mercedes</a> | <a href="cars.php?make=Suzuki&amp;item=car" id="makeA_8">Suzuki</a> | <a href="cars.php?make=Mitsubishi&amp;item=car" id="makeA_9">Mitsubishi</a> | <a href="cars.php?make=Jeep&amp;item=car" id="makeA_10">Jeep</a> | <a href="cars.php?make=BMW&amp;item=car" id="makeA_11">BMW</a> | <a href="cars.php?make=Volkswagen&amp;item=car" id="makeA_12">Volkswagen.</a>
+<div class="row blue" id="searchbymake" style="margin-top: 20px;   width: 90%;">
+    <h4></h4>
+    <p id="makeP_1" sytle="font-size: 20px !important;">
+        <a href="cars.php?make=Subaru&amp;item=car" id="makeA_13">Search by make >></a>
+        <a href="cars.php?make=Subaru&amp;item=car" id="makeA_2">Subaru</a> | <a href="cars.php?make=Toyota&amp;item=car" id="makeA_3">Toyota</a> | <a href="cars.php?make=Jaguar&amp;item=car" id="makeA_4">Jaguar</a> | <a href="cars.php?make=Nissan&amp;item=car" id="makeA_5">Nissan</a> | <a href="cars.php?make=Chevrolet&amp;item=car" id="makeA_6">Chevrolet</a> | <a href="cars.php?make=Mercedes&amp;item=car" id="makeA_7">Mercedes</a> | <a href="cars.php?make=Suzuki&amp;item=car" id="makeA_8">Suzuki</a> | <a href="cars.php?make=Mitsubishi&amp;item=car" id="makeA_9">Mitsubishi</a> | <a href="cars.php?make=Jeep&amp;item=car" id="makeA_10">Jeep</a> | <a href="cars.php?make=BMW&amp;item=car" id="makeA_11">BMW</a> | <a href="cars.php?make=Volkswagen&amp;item=car" id="makeA_12">Volkswagen</a>
     </p>
 </div>
