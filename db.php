8068d28110a257a1c6f4b57162315fd5220e3426
Daniel <Daniel@DANIEL-PC>
2017-06-27 09:44:05 +0300

    Changed by Daniel

--- db.php
+++ db.php
@@ -23,13 +23,19 @@ class SmartcarDB extends mysqli {
         trigger_error('Deserializing is not allowed.', E_USER_ERROR);
     }
 
-    private function __construct() {
-        parent::__construct($this->dbHost, $this->user, $this->pass, $this->dbName);
+  private function __construct() {
+define('DB_USERNAME', 'root');//a10a1d_aiddist
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'smartcar_vehicles');
+        $this->con = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
         if (mysqli_connect_error()) {
             exit('Connect Error (' . mysqli_connect_errno() . ') '
                     . mysqli_connect_error());
         }
-        parent::set_charset('utf-8');
+        
+         return $this->con;
+        //parent::set_charset('utf-8');
     }
 
     public function create_subscription($email) {
@@ -38,7 +44,41 @@ class SmartcarDB extends mysqli {
     }
 
     public function get_insurors() {
-        return $this->query("SELECT * FROM insurance_agentsHide");
+        return $this->query("SELECT * FROM insurance_agents");
+    }
+    
+     public function get_all_cars_sale() {
+        //return $this->query("SELECT * FROM car WHERE (car_for='Sale' AND display_status=1)");
+        $sale = 'Sale';
+        //echo $sale;
+           $stmt = $this->con->prepare("SELECT * FROM car WHERE car_for=? AND display_status=1");
+        $stmt->bind_param("s",$sale);
+       $stmt->execute();
+        $result = $stmt->get_result();
+        //$stmt->close();
+        return $result;
+        
+    }
+    
+       public function get_all_cars_hire() {
+        //return $this->query("SELECT * FROM car WHERE (car_for='Hire' AND display_status=1)");
+        $Hire = 'Hire';
+           $stmt = $this->con->prepare("SELECT * FROM car WHERE car_for=? AND display_status=1");
+        $stmt->bind_param("s",$Hire);
+       $stmt->execute();
+        $result = $stmt->get_result();
+        //$stmt->close();
+        return $result;
+    }
+      public function get_car_by_make($make) {
+          //$make1 = $this->real_escape_string($make);        
+        //return $this->query("SELECT * FROM car WHERE make=$make1");
+           $stmt = $this->con->prepare("SELECT * FROM car WHERE make=?");
+        $stmt->bind_param("s",$make);
+       $stmt->execute();
+        $result = $stmt->get_result();
+        //$stmt->close();
+        return $result;
     }
 
 }
